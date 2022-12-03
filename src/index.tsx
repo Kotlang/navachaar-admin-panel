6bfdf5ed82cfccedec8cfd5b7d39684f017ca422
Vikash Gupta <21vikashgupta@gmail.com>
2022-12-03 20:16:49 +0530

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
-import { ProSidebarProvider } from "react-pro-sidebar";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,9 +13,7 @@ const router = createBrowserRouter(routes);
 
 root.render(
   <React.StrictMode>
-    <ProSidebarProvider>
-      <RouterProvider router={router} />
-    </ProSidebarProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 

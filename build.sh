01f2d67cd8d706b859586db32df9826381fb55a5
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-01-01 14:52:36 +0530

    fix: build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 rm -Rf src/generated
 mkdir src/generated
-cd ./social-model
+cd social-model
 protoc --js_out=import_style=commonjs,binary:../src/generated \
     --grpc-web_out=import_style=typescript,mode=grpcwebtext:../src/generated \
     *.proto
 cd ..
 
-cd ./auth-model
+cd auth-model
 protoc --js_out=import_style=commonjs,binary:../src/generated \
     --grpc-web_out=import_style=typescript,mode=grpcwebtext:../src/generated \
     *.proto

55a1f38839985215444af34a534043924210f155
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-01-07 09:52:41 +0530

    chore(Rename): project renamed

--- src/ui-components/CustomIcons.tsx
+++ src/ui-components/CustomIcons.tsx
@@ -1,11 +1,11 @@
-// Copyright 2022-2023 @Kotlang/navachar-admin-portal authors & contributors
+// Copyright 2022-2023 @Kotlang/navachaar-admin-portal authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import Icon from '@ant-design/icons';
 import type { CustomIconComponentProps } from '@ant-design/icons/lib/components/Icon';
 import React from 'react';
-import { ReactComponent as NavaCharSVG } from 'src/assets/icons/navachar.svg';
+import { ReactComponent as NavaCharSVG } from 'src/assets/icons/navachaar.svg';
 
 export const NavaCharIcon = (props: Partial<CustomIconComponentProps>) => (
 	<Icon component={NavaCharSVG} {...props} />

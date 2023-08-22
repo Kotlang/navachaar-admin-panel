8cc6a29e143b7b160a8c9f50a3caf5621420d0b0
Prince <this.is.kumar.prince@gmail.com>
2023-08-22 22:58:54 +0530

    feat: docker image config

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,12 +2,13 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import { Gender } from 'src/generated/common_pb';
 export interface IUserProfile {
 	attributesList?: string[];
 	createdOn?: number;
 	domain?: string;
 	email?: string;
-	gender?: string;
+	gender?: Gender;
 	loginId?: string;
 	metaData?: string;
 	name?: string;
